47e1774e6b1f1e9dbfacc9d0c8d01b0d81a8c8bd
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-12-01 15:43:23 -0800

    added app.post and app.get for note taking and viewing

--- server.js
+++ server.js
@@ -6,21 +6,48 @@ const PORT = 3001;
 
 const app = express();
 
-app.use(express.static('public'))
+const {v4:uuidv4} = require('uuid');
 
-// app.use(express.urlencoded({ extended: true }));
+const fs = require('fs');
 
-// app.use(express.json());
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 
 // create routes that will serve our static html files
 
-app.get('/home', (req, res) =>  
+app.get('/', (req, res) =>  
     res.sendFile(path.join(__dirname, 'public/index.html'))
 )
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 )
+
+app.get('/api/notes',(req, res)=>{
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    err ? console.log(err) :  res.json(JSON.parse(data))
+  })}
+
+)
+
+app.post('/api/notes', (req, res)=>{
+  let title = req.body.title;
+  let text = req.body.text;
+  let newNote = {title, text, id:uuidv4()};
+  
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    let currentNotes = JSON.parse(data);
+    currentNotes.push(newNote)
+    fs.writeFile('db/db.json', JSON.stringify(currentNotes), (err) => {
+      err ? console.log(err) : console.log('This note has been saved' + newNote.title)
+    })
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+  })
+})
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
