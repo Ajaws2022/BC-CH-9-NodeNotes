3120bbf5135cc6cd73740391c02e27551f366c3e
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-12-01 16:00:23 -0800

    created objects in "get"

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(express.json());
 // create routes that will serve our static html files
 
 app.get('/', (req, res) =>  
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    {res.sendFile(path.join(__dirname, 'public/index.html'))}
 )
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    {res.sendFile(path.join(__dirname, 'public/notes.html'))}
 )
 
 app.get('/api/notes',(req, res)=>{
