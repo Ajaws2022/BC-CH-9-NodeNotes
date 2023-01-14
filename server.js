be012e37fdeefdc317109946835c7a8a94cf1a33
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-01-14 14:25:51 -0800

    debugged and completed delete route

--- server.js
+++ server.js
@@ -50,6 +50,24 @@ app.post('/api/notes', (req, res)=>{
   })
 })
 
+app.delete('/api/notes/:id', (req, res)=>{
+  let id = req.params.id;
+
+  // console.log(id)
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    let currentNotes = JSON.parse(data);
+    const index = currentNotes.findIndex(object => {
+      return object.id === id;
+    });
+    console.log( 'first step', currentNotes)
+    currentNotes.splice(index, 1);
+    fs.writeFile('db/db.json', JSON.stringify(currentNotes), (err) => {
+      err ? console.log(err) : console.log('This note has been deleted')
+    })
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+  })
+
+})
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
